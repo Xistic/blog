6cee9a35a34f7adf57cf1106b3d53b4e96f0b1d6
Xistic <57176365+Xistic@users.noreply.github.com>
2023-08-01 14:31:13 +0300

    Delete myblog directory

--- myblog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Main.as_view(), name='main_page'),
-    path('notes', views.NoteView.as_view(), name='notes_page'),
-    path('resume', views.ResumeView.as_view(), name='resume_page'),
-    path('roadmap/', views.Roadmap.as_view(), name='roadmap_page'),
-]
